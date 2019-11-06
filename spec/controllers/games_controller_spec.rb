747a39dee11bf0f69a169c64c0591d6f7b617559
Sloane <sloanems@aol.com>
2019-11-06 22:19:50 +0000

    created the games_controller_spec.rb file and created passing index test

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe GamesController, type: :controller do
   describe "games#index action" do
     it "should successfully show the page" do
       get :index
+      expect(response).to have_http_status(:success)
     end
   end
 end
