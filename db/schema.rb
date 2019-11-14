f795164e38b93bddf4f79038be45cbe5d6ae0911
Sloane <sloanems@aol.com>
2019-11-14 18:10:19 +0000

    test to fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_220634) do
+ActiveRecord::Schema.define(version: 2019_11_13_220719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,11 @@ ActiveRecord::Schema.define(version: 2019_11_13_220634) do
     t.string "color"
     t.integer "game_id"
     t.boolean "captured?"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "queens", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
