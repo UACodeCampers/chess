51fa9bbc4045ac3028fc0ca11225f47f72752fb8
Devoun <THREExPENNYco@gmail.com>
2019-11-20 07:58:14 +0000

    fix to Pawn valid_move?

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -4,7 +4,7 @@ class Pawn < Piece
         self.x_position == self.starting_position_x && self.y_position == self.starting_position_y ? distance = 2 : distance = 1 
         # checks if y_position is increasing or decreasing by more than 1
         return self.invalid_move(new_x, new_y) if new_y > 8
-        return self.invalid_move(new_x, new_y) if new_y > self.y_position + distance || new_y < self.x_position
+        return self.invalid_move(new_x, new_y) if new_y > self.y_position + distance || new_y < self.y_position
         # if distance is valid checks if pawn is obstructed
         return self.is_obstructed?(new_x, new_y)
     end
