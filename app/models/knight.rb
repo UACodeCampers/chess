c55bf3254b09de895a3b2954ead7ee2c39d27c97
Devoun <THREExPENNYco@gmail.com>
2019-11-21 02:13:20 +0000

    knight logic added for valid move

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,14 @@
 class Knight < Piece
+    def valid_move?
+        if (new_x - self.x_position).abs == 2
+            return self.invalid_move(new_x, new_y) if (new_y - self.y_position).abs > 1
+        elsif (new_y - self.y_position).abs == 2
+            return self.invalid_move(new_x, new_y) if (new_x - self.x_position).abs > 1
+        else 
+            return self.invalid_move(new_x, new_y) 
+        end 
+
+        return self.invalid_move(new_x, new_y) if new_x > 8 || new_y > 8 
+        return self.invalid_move(new_x, new_y) if new_y < 1 || new_y < 1 
+    end
 end
