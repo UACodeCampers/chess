34289cf548493ba1804b06b2c48dac50784959a7
Devoun <THREExPENNYco@gmail.com>
2019-11-21 23:46:23 +0000

    adding comments for clarity

--- app/models/king.rb
+++ app/models/king.rb
@@ -2,6 +2,8 @@ class King < Piece
     def valid_move?(new_x, new_y)
         distance = 1 # sets diastance for all king moves
         # checks if y_position or x_position is increasing or decreasing by more than 1
+        return self.invalid_move(new_x, new_y) if new_x > 8 || new_x < 1
+        return self.invalid_move(new_x, new_y) if new_y > 8 || new_y < 1 
         return self.invalid_move(new_x, new_y) if new_x > self.x_position + distance || new_x < self.x_position - distance 
         return self.invalid_move(new_x, new_y) if new_y > self.y_position + distance || new_y < self.y_position - distance 
         # if distance is valid checks if king is obstructed
