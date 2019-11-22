e65f2487d89fbcabb033aafdf69c9c2dbda26810
Devoun <THREExPENNYco@gmail.com>
2019-11-22 05:33:20 +0000

    adding rspec tests

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,6 @@ class GamesController < ApplicationController
 
   def index
     @available_games = Game.available.order("created_at DESC")
-
   end
 
   def new
---
+++ spec/models/piece_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Piece, type: :model do
+# describe "is_obstructed?" do
+#  it "should detect vertical obstructions" do 
+#   piece = piece
+end
