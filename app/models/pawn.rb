7c8eec5e768e322c00ba3a8ce5be8ea55a935d51
Devoun <THREExPENNYco@gmail.com>
2019-11-22 08:13:42 +0000

    adjustment to pawn model

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -4,14 +4,14 @@ class Pawn < Piece
             # checks if pawn is on starting position returns distance 
             self.x_position == self.starting_position_x && self.y_position == self.starting_position_y ? distance = 2 : distance = 1 
         # checks if y_position is increasing or decreasing by more than 1
-            return self.invalid_move(new_x, new_y) if new_y > 8
+            return self.invalid_move(new_x, new_y) if new_y > 8 || new_y < 1
             return self.invalid_move(new_x, new_y) if new_y > self.y_position + distance || new_y < self.y_position
         # if distance is valid checks if pawn is obstructedS
             return self.is_obstructed?(new_x, new_y)
         elsif self.color == "black"
             self.x_position == self.starting_position_x && self.y_position == self.starting_position_y ? distance = 2 : distance = 1
-            return self.invalid_move(new_x, new_y) if new_y < 1
-            return self.invalid_move(new_x, new_y) if new_y < self.y_position + distance || new_y > self.y_position
+            return self.invalid_move(new_x, new_y) if new_y > 8 || new_y < 1 
+            return self.invalid_move(new_x, new_y) if new_y < self.y_position - distance || new_y > self.y_position
             return self.is_obstructed?(new_x, new_y)
         end 
     end
