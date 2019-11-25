1b7090f826e5f615b1655f8633530d97e2f3c3a2
Wilson <wilsonhorrell@git.training>
2019-11-25 11:25:20 -0600

    Bringing up to date

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,29 +8,29 @@ class Game < ApplicationRecord
         where(black_player_id: nil).or(where(white_player_id: nil))
     end 
 
-#   def populate_game!
+def populate_game!
       
-#   #WHITE PIECES
-#     Pawn.create(1..8).each do |i| Pawn.create(game_id: id, starting_position_x: 'i', starting_position_y: 1, color: white, user_id: white_player_id, piece_type: Pawn) end
-#     Rook.create(game_id: id, starting_position_x: 1, starting_position_y: 1, color: white, user_id: white_player_id, piece_type: Rook)
-#     Rook.create(game_id: id, starting_position_x: 1, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Rook)
-#     Knight.create(game_id: id, starting_position_x: 2, starting_position_y: 2, color: white, user_id: white_player_id, piece_type: Knight)
-#     Knight.create(game_id: id, starting_position_x: 7, starting_position_y: 1, color: white, user_id: white_player_id, piece_type: Knight)
-#     Bishop.create(game_id: id, starting_position_x: 3, starting_position_y: 1, color: white, user_id: white_player_id, piece_type: Bishop)
-#     Bishop.create(game_id: id, starting_position_x: 6, starting_position_y: 1,  color: white, user_id: white_player_id, piece_type: Bishop)
-#     Queen.create(game_id: id, starting_position_x: 4, starting_position_y: 1, color: white, user_id: white_player_id, piece_type: Queen)
-#     King.create(game_id: id, starting_position_x: 5, starting_position_y: 1, color: white, user_id: white_player_id, piece_type: King)
+   #WHITE PIECES
+     Pawn.create(1..8).each do |i| Pawn.create(game_id: id, starting_position_x: 'i', starting_position_y: 1, color: white, user_id: white_player_id, piece_type: Pawn) end
+     Rook.create(game_id: id, starting_position_x: 1, starting_position_y: 1, color: white, user_id: white_player_id, piece_type: Rook)
+     Rook.create(game_id: id, starting_position_x: 1, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Rook)
+     Knight.create(game_id: id, starting_position_x: 2, starting_position_y: 2, color: white, user_id: white_player_id, piece_type: Knight)
+     Knight.create(game_id: id, starting_position_x: 7, starting_position_y: 1, color: white, user_id: white_player_id, piece_type: Knight)
+     Bishop.create(game_id: id, starting_position_x: 3, starting_position_y: 1, color: white, user_id: white_player_id, piece_type: Bishop)
+     Bishop.create(game_id: id, starting_position_x: 6, starting_position_y: 1,  color: white, user_id: white_player_id, piece_type: Bishop)
+     Queen.create(game_id: id, starting_position_x: 4, starting_position_y: 1, color: white, user_id: white_player_id, piece_type: Queen)
+     King.create(game_id: id, starting_position_x: 5, starting_position_y: 1, color: white, user_id: white_player_id, piece_type: King)
   
 
 # #BLACK PIECES
-#     Pawn.create(1..8).each do |i| Pawn.create(game_id: id, starting_position_x: 'i', starting_position_y: 7, color: white, user_id: white_player_id, piece_type: Pawn) end
-#     Rook.create(game_id: id, starting_position_x: 1, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Rook)
-#     Rook.create(game_id: id, starting_position_x: 8, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Rook)
-#     Knight.create(game_id: id, starting_position_x: 2, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Knight)
-#     Knight.create(game_id: id, starting_position_x: 7, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Knight)
-#     Bishop.create(game_id: id, starting_position_x: 3, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Bishop)
-#     Bishop.create(game_id: id, starting_position_x: 6, starting_position_y: 8,  color: white, user_id: white_player_id, piece_type: Bishop)
-#     Queen.create(game_id: id, starting_position_x: 4, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Queen)
-#     King.create(game_id: id, starting_position_x: 5, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: King)
-#   end
+    Pawn.create(1..8).each do |i| Pawn.create(game_id: id, starting_position_x: 'i', starting_position_y: 7, color: white, user_id: white_player_id, piece_type: Pawn) end
+     Rook.create(game_id: id, starting_position_x: 1, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Rook)
+     Rook.create(game_id: id, starting_position_x: 8, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Rook)
+     Knight.create(game_id: id, starting_position_x: 2, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Knight)
+     Knight.create(game_id: id, starting_position_x: 7, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Knight)
+     Bishop.create(game_id: id, starting_position_x: 3, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Bishop)
+     Bishop.create(game_id: id, starting_position_x: 6, starting_position_y: 8,  color: white, user_id: white_player_id, piece_type: Bishop)
+     Queen.create(game_id: id, starting_position_x: 4, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: Queen)
+     King.create(game_id: id, starting_position_x: 5, starting_position_y: 8, color: white, user_id: white_player_id, piece_type: King)
+   end
 end
