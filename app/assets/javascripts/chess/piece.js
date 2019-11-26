1ede37b5de09452a8eaf6496f55a2de4e3ff169e
knorthrup83 <knorthrup83@gmail.com>
2019-11-26 07:39:55 -0700

    Added notes in pieces.js file (may not keep)

---
+++ app/assets/javascripts/chess/piece.js
@@ -0,0 +1,20 @@
+// function Piece(name, x, y) {
+//     this.name = name
+//     this.x
+//     this.y
+// }
+// Piece.prototype.move = function () {
+//   // AJAX call
+// }
+// <td>
+//   <div id="1" class="piece rook" data-name="rook" data-x="1" data-y="1"></div>
+// </td>
+
+// $('.piece').each(function (piece) {
+//     var myPiece = new Piece(piece.data('name'), piece.data('x'), piece.data('y')));
+//     piece.draggable({
+//         stop: function(event, ui) {
+//             piece.move();
+//         }
+//     })
+// });
