febfdc65ab6b56ecf1f5f6171e02c09cde97008c
Wilson <wilsonhorrell@git.training>
2019-11-25 18:32:01 -0600

    Pieces on board and ajax updating

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,21 +1,25 @@
 class PiecesController < ApplicationController
 
-def create
-    @piece = game.pieces.create(piece_params)
-    
-      redirect_to games_path(@game)
-  end
-def show
+ def update
     @piece = Piece.find(params[:id])
     @game = @piece.game
-    @pieces = @game.pieces
-end
-private
-	def piece_params
-    params.permit(
-      :game_id, :x_position, :y_position, :starting_position_x, :starting_position_y, :color, :piece_type, :html_code, :captured?)
 
+    
+
+      @piece.update(piece_params)
+      respond_to do |format|
+        format.html { render :show }
+        format.json { render json: @piece, status: :ok}
+      end
+    
+    end
+
+    private 
+    def piece_params
+    params.permit(:game_id, :starting_position_x, :starting_position_y, :color, :piece_type, :html_code)
   end
-end
+    
+    
+  end
 
  
