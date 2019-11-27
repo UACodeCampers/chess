de1738cdfdf85730ae0b76acb1800c05afdbbce1
knorthrup83 <knorthrup83@gmail.com>
2019-11-27 11:16:23 -0700

    Removed extra field (html_code) from pieces table

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2019_11_25_175508) do
     t.boolean "captured?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "html_code"
   end
 
   create_table "users", force: :cascade do |t|
