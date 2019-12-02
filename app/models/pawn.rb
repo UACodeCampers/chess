80dfe150ce6383a4aa5729bdef464297c7418992
Devoun <THREExPENNYco@gmail.com>
2019-12-02 07:23:28 +0000

    changes to pawn model

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,18 +1,29 @@
 class Pawn < Piece
     def valid_move?(new_x, new_y)   
         return self.invalid_move(new_x, new_y) if new_y > 8 || new_y < 1 
-        return self.invalid_move(new_x, new_y) if new_x > 8 || new_y < 1 
+        return self.invalid_move(new_x, new_y) if new_x > 8 || new_x < 1 
         if self.color == "white"
-            # checks if pawn is on starting position returns distance 
+        # checks if pawn is on starting position returns distance 
             self.x_position == self.starting_position_x && self.y_position == self.starting_position_y ? distance = 2 : distance = 1 
         # checks if y_position is increasing or decreasing by more than 1
             return self.invalid_move(new_x, new_y) if new_y > self.y_position + distance || new_y < self.y_position
-        # if distance is valid checks if pawn is obstructedS
-            return self.is_obstructed?(new_x, new_y)
+        #checks to see if there is a piece vertical
+            return self.invalid_move(new_x, new_y) if Piece.exist?(
+                x_position: new_x, 
+                y_position: new_y, 
+                game_id: self.game_id, 
+                color: "black"
+            )
+        # if distance is valid checks if pawn is obstructed
         elsif self.color == "black"
             self.x_position == self.starting_position_x && self.y_position == self.starting_position_y ? distance = 2 : distance = 1
             return self.invalid_move(new_x, new_y) if new_y < self.y_position - distance || new_y > self.y_position
-            return self.is_obstructed?(new_x, new_y)
-        end 
+            return self.invalid_move(new_x, new_y) if Piece.exist?(
+                x_position: new_x,
+                y_position: new_y,
+                game_id: self.game_id,
+                color: "white"
+            )
+        end         
     end
 end
