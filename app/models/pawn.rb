3367569330d1a0c9b00ecf99bc1e9de324f2dd7e
Devoun <THREExPENNYco@gmail.com>
2019-12-03 04:25:36 +0000

    changes to pawn model to add capture logic for pawn

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -12,9 +12,13 @@ class Pawn < Piece
                 x_position: new_x, 
                 y_position: new_y, 
                 game_id: self.game_id, 
-                color: "black"
             )
-        # if distance is valid checks if pawn is obstructed
+            return true if Piece.exist?( 
+                x_position: self.x_position + 1 || self.x_position - 1, 
+                y_position: self.y_position + 1, 
+                game_id: self.game_id,
+                color: "black" 
+            ) 
         elsif self.color == "black"
             self.x_position == self.starting_position_x && self.y_position == self.starting_position_y ? distance = 2 : distance = 1
             return self.invalid_move(new_x, new_y) if new_y < self.y_position - distance || new_y > self.y_position
@@ -22,8 +26,13 @@ class Pawn < Piece
                 x_position: new_x,
                 y_position: new_y,
                 game_id: self.game_id,
+            )
+            return true if Piece.exist?(
+                x_position: self.x_position + 1 || self.x_position - 1, 
+                y_position: self.y_position - 1, 
+                game_id: self.game_id, 
                 color: "white"
             )
-        end         
+        end    
     end
 end
