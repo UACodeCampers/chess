f1b1297b54351086a4c3d0a4a5e997706a590c57
Devoun <THREExPENNYco@gmail.com>
2019-12-04 06:43:22 +0000

    adding more rspec test for is_obstructed

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -25,6 +25,17 @@ RSpec.describe Piece, type: :model do
       expect(piece.is_obstructed?(5, 6)).to be_truthy
     end 
 
+    it "should detect diagonal obstruction" do 
+      piece = Piece.create(x_position: 5, y_position: 5, piece_type: "Bishop", color: "black", game_id: 1)
+      piece2 = Piece.create(x_position: 7, y_position: 7, piece_type: "Pawn", color: "white", game_id: 1)
+      expect(piece.is_obstructed?(8, 8)).to be_truthy 
+    end 
+
+    it "should detect diagonal obstruction" do 
+      piece = Piece.create(x_position: 2, y_position: 2, piece_type: "Queen", color: "white", game_id: 1)
+      piece2 = Piece.create(x_position: 1, y_position: 1, piece_type: "Pawn", color: "black", game_id: 1)
+      expect(piece.is_obstructed?(4, 4)).to be_falsey
+    end 
 
   end 
 end
