42971999c2cdcb37b13b0db16e4a540bfeb19165
knorthrup83 <knorthrup83@gmail.com>
2019-12-06 11:06:03 -0700

    Created pieces controller

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,13 +1,14 @@
 class PiecesController < ApplicationController
     before_action :authenticate_user!
     def update
-        piece = Piece.find(params[:id])
+        piece = Piece.find(params[:id]).transmogrify
         x = params[:x_position]
         y = params[:y_position]
         begin 
             piece.move_to!(x, y)
         rescue RuntimeError => e
             render json: {success: false, message: e.message}
+            return
         end
         render json: {success: true}
     end
