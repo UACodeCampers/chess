0473b7773b096c85a82b2f1723d6309a8ce0d9e1
knorthrup83 <knorthrup83@gmail.com>
2019-12-06 13:53:22 -0700

    Working on pieces_controller #update

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,15 +1,18 @@
 class PiecesController < ApplicationController
     before_action :authenticate_user!
     def update
-        piece = Piece.find(params[:id])
+        @piece = Piece.find(params[:id])
         x = params[:x_position]
         y = params[:y_position]
         begin 
-            piece.move_to!(x, y)
+            @piece.move_to!(x, y)
         rescue RuntimeError => e
             render json: {success: false, message: e.message}
             return
         end
         render json: {success: true}
+        @piece.update_attributes(:x_position => x, :y_position => y)
+        redirect_to game_path
     end
+
 end
