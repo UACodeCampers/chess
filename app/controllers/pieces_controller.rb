7141717b3a2c48461b4b1f7870a84f94b86ffabe
knorthrup83 <knorthrup83@gmail.com>
2019-12-06 09:35:28 -0700

    Added pieces controller

---
+++ app/controllers/pieces_controller.rb
@@ -0,0 +1,14 @@
+class PiecesController < ApplicationController
+    before_action :authenticate_user!
+    def update
+        piece = Piece.find(params[:id])
+        x = params[:x_position]
+        y = params[:y_position]
+        begin 
+            piece.move_to!(x, y)
+        rescue RuntimeError => e
+            render json: {success: false, message: e.message}
+        end
+        render json: {success: true}
+    end
+end
