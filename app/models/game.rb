47e0c6ed52d629d12185f47d045f7f7b6efed02d
knorthrup83 <knorthrup83@gmail.com>
2019-12-06 11:10:13 -0700

    Commented out associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,8 @@
 class Game < ApplicationRecord
   has_many :pieces
 
-  belongs_to :black_player, class_name: 'User', foreign_key: 'black_player_id'
-  belongs_to :white_player, class_name: 'User', foreign_key: 'white_player_id'
+  # belongs_to :black_player, class_name: 'User', foreign_key: 'black_player_id'
+  # belongs_to :white_player, class_name: 'User', foreign_key: 'white_player_id'
 
   after_create :populate_game!
   
