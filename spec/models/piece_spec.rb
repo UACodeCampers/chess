b09799a693a70422bdd3de9adbb6cec641d1cc61
knorthrup83 <knorthrup83@gmail.com>
2019-12-07 10:31:52 -0700

    Fixed piece_spec conflict

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -2,28 +2,38 @@ require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
 
-
   describe "is_obstructed?" do
-  let!(:user1) {FactoryBot.create :user}
-  let!(:user2) {FactoryBot.create :user}
-  let!(:game1) { Game.create(id: 1, name: "game1", black_player_id: 3, white_player_id: 2)}
-  let!(:piece1) { Game.find(1).pieces.create(x_position: 5, y_position: 7, piece_type: "Pawn", color: "black", game_id: 1)}
-  let!(:piece2) { Game.find(1).pieces.create(x_position: 5, y_position: 3, piece_type: "Pawn", color: "black", game_id: 1)}
-  let!(:piece3) { Game.find(1).pieces.create(x_position: 5, y_position: 2, piece_type: "Pawn", color: "black", game_id: 1)}
-
-
-    it "should detect downward vertical obstructions" do 
-      piece = Piece.new(x_position: 5, y_position: 5, piece_type: "Rook", color: "black", game_id: 1)
-      expect(piece.is_obstructed?(5, 3)).to be_falsey
+    it "should detect upward vertical obstructions" do 
+      user1 = User.create(id: 3, name: "David", email: "richardtracy@yahoo.com", password: "123456")
+      game1 = Game.create(id: 4563, name: "Game with tim", white_player_id: user1.id)
+      piece = Piece.create(x_position:5, y_position: 2, piece_type: "Rook", color: "white", game_id: 4563)
+      piece2 = Piece.create(x_position: 5, y_position: 5, piece_type: "Rook", color: "black", game_id: 4563)
+      expect(piece.is_obstructed?(5, 1)).to be_falsey
     end 
-
     
     it "should detect downward vertical obstructions" do 
-      piece = Piece.new(x_position: 5, y_position: 5, piece_type: "Rook", color: "white", game_id: 1)
-      expect(piece.is_obstructed?(5, 2)).to be_truthy
+      user1 = User.create(id: 3, name: "David", email: "richardtracy@yahoo.com", password: "123456")
+      game1 = Game.create(id: 4563, name: "Game with tim", white_player_id: user1.id)
+      piece = Piece.create(x_position: 5, y_position: 8, piece_type: "Rook", color: "white", game_id: 4563)
+      piece2 = Piece.create(x_position: 5, y_position: 7, piece_type: "Queen", color: "black", game_id: 4563)
+      expect(piece.is_obstructed?(5, 6)).to be_truthy
     end 
 
+    it "should detect diagonal obstruction" do 
+      user1 = User.create(id: 3, name: "David", email: "richardtracy@yahoo.com", password: "123456")
+      game1 = Game.create(id: 4563, name: "Game with tim", white_player_id: user1.id)
+      piece = Piece.create(x_position: 5, y_position: 5, piece_type: "Bishop", color: "black", game_id: 4563)
+      piece2 = Piece.create(x_position: 7, y_position: 7, piece_type: "Pawn", color: "white", game_id: 4563)
+      expect(piece.is_obstructed?(8, 8)).to be_truthy 
+    end 
 
+    it "should detect diagonal obstruction" do 
+      user1 = User.create(id: 3, name: "David", email: "richardtracy@yahoo.com", password: "123456")
+      game1 = Game.create(id: 4563, name: "Game with tim", white_player_id: user1.id)
+      piece = Piece.create(x_position: 2, y_position: 2, piece_type: "Queen", color: "white", game_id: 4563)
+      piece2 = Piece.create(x_position: 1, y_position: 1, piece_type: "Pawn", color: "black", game_id: 4563)
+      expect(piece.is_obstructed?(4, 4)).to be_falsey
+    end 
   end 
 
   describe "occupied?" do
