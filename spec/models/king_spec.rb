ef8186aba530654a8e9b542aecee736680912c04
Devoun <THREExPENNYco@gmail.com>
2019-12-09 03:35:06 +0000

    renaming test

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe King, type: :model do
       expect(piece.check?(7, 4)).to be_truthy
     end 
 
-    it "should detect valid move for king" do 
+    it "should detect if a King is in check" do 
       user1 = User.create(id: 3, name: "David", email: "richardtracy@yahoo.com", password: "123456")
       game1 = Game.create(id: 4563, name: "Game with tim", white_player_id: user1.id)
       piece = Piece.create(x_position: 3, y_position: 3, piece_type: "King", color: "white", game_id: 4563, captured?: "false")
@@ -55,7 +55,7 @@ RSpec.describe King, type: :model do
       expect(piece.check?(4, 3)).to be_truthy
     end 
 
-    it "should detect valid move for king" do 
+    it "should detect if a King is in check" do 
       user1 = User.create(id: 3, name: "David", email: "richardtracy@yahoo.com", password: "123456")
       game1 = Game.create(id: 4563, name: "Game with tim", white_player_id: user1.id)
       piece = Piece.create(x_position: 3, y_position: 3, piece_type: "King", color: "white", game_id: 4563, captured?: "false")
@@ -63,7 +63,7 @@ RSpec.describe King, type: :model do
       expect(piece.check?(4, 4)).to be_falsey
     end 
 
-    it "should detect valid move for king" do 
+    it "should detect if a King is in check" do 
       user1 = User.create(id: 3, name: "David", email: "richardtracy@yahoo.com", password: "123456")
       game1 = Game.create(id: 4563, name: "Game with tim", white_player_id: user1.id)
       piece = Piece.create(x_position: 3, y_position: 1, piece_type: "King", color: "white", game_id: 4563, captured?: "false")
