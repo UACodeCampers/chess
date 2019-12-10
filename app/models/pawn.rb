0d59eec1e20bcafa54711959f9815ba7794aa35c
Devoun <THREExPENNYco@gmail.com>
2019-12-10 04:57:40 +0000

    more refactor

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -6,12 +6,12 @@ class Pawn < Piece
         self.x_position == self.starting_position_x && self.y_position == self.starting_position_y ? distance = 2 : distance = 1 
         if self.color == "white"
             return self.invalid_move(new_x, new_y) if new_y > self.y_position + distance || new_y < self.y_position
-                if Piece.exists?(
+                if Game.find(self.game_id).pieces.exists?(
                     x_position: new_x, 
                     y_position: new_y, 
                     game_id: self.game_id, 
                 ) 
-                    piece = Piece.find_by(x_position: new_x, y_position: new_y, game_id: self.game_id)
+                    piece = Game.find(self.game_id).pieces.find_by(x_position: new_x, y_position: new_y, game_id: self.game_id)
                     return self.invalid_move(new_x, new_y) if piece.color == self.color
                     return self.invalid_move(new_x, new_y) if piece.x_position == self.x_position
                     return true if (new_x - self.x_position).abs && (new_y - self.y_position).abs == 1
@@ -20,12 +20,12 @@ class Pawn < Piece
             return true 
         elsif self.color == "black"
             return self.invalid_move(new_x, new_y) if new_y < self.y_position - distance || new_y > self.y_position
-                if Piece.exists?(
+                if Game.find(self.game_id).pieces.exists?(
                     x_position: new_x, 
                     y_position: new_y, 
                     game_id: self.game_id, 
                 ) 
-                    piece = Piece.find_by(x_position: new_x, y_position: new_y, game_id: self.game_id)
+                    piece = Game.find(self.game_id).pieces.find_by(x_position: new_x, y_position: new_y, game_id: self.game_id)
                     return self.invalid_move(new_x, new_y) if piece.color == self.color
                     return self.invalid_move(new_x, new_y) if piece.x_position == self.x_position
                     return true if (new_x - self.x_position).abs && (new_y - self.y_position).abs == 1
